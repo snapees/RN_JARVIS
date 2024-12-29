605402cd32298983164ecb6662bf18a079c02750
snapees <93643707+snapees@users.noreply.github.com>
2024-12-30 05:16:14 +0530

    `Added pedometer feature with step counter and progress indicator`

--- src/components/pedometer/Pedometer.tsx
+++ src/components/pedometer/Pedometer.tsx
@@ -1,11 +1,94 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import {Colors} from '../../utils/Constants';
+/* eslint-disable react-hooks/exhaustive-deps */
+import {Alert, Image, StyleSheet, TouchableOpacity, View} from 'react-native';
+import React, {FC, useEffect} from 'react';
+import {Colors, Fonts} from '../../utils/Constants';
+import {RFValue} from 'react-native-responsive-fontsize';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {usePedometerStore} from '../../state/pedometerStore';
+import StepCounter, {
+  parseStepData,
+  startStepCounterUpdate,
+  stopStepCounterUpdate,
+} from '@dongminyu/react-native-step-counter';
+import {playTTS} from '../../utils/ttsListeners';
+import CircularProgress from 'react-native-circular-progress-indicator';
+import CustomText from '../ui/CustomText';
 
-const Pedometer = () => {
+interface pedometerProps {
+  message: string;
+  onCross: () => void;
+}
+
+const Pedometer: FC<pedometerProps> = ({message, onCross}) => {
+  const {stepCount, dailyGoal, addSteps} = usePedometerStore();
+  StepCounter.addListener('StepCounter.stepsSensorInfo');
+
+  const startStepCounter = () => {
+    startStepCounterUpdate(new Date(), data => {
+      const parsedData = parseStepData(data);
+      addSteps(parsedData.steps, parsedData.distance);
+    });
+  };
+
+  const stopStepCounter = () => {
+    stopStepCounterUpdate();
+  };
+
+  useEffect(() => {
+    if (stepCount >= dailyGoal) {
+      playTTS(
+        "You've met your daily goal. No need to start the counter again today.",
+      );
+    } else {
+      startStepCounter();
+    }
+
+    return () => {
+      stopStepCounter();
+    };
+  }, []);
+
   return (
-    <View>
-      <Text>Pedometer</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.cross}
+        onPress={() => {
+          Alert.alert('Your step counter stopped!!');
+          stopStepCounter();
+          onCross();
+        }}>
+        <Icon name="close-circle" color="red" size={RFValue(20)} />
+      </TouchableOpacity>
+      <Image
+        source={require('../../assets/images/logo_short.png')}
+        style={styles.logo}
+      />
+
+      <View style={styles.indicator}>
+        <CircularProgress
+          value={stepCount}
+          maxValue={dailyGoal}
+          valueSuffix="/2000"
+          progressValueFontSize={22}
+          radius={120}
+          activeStrokeColor="#cdd27e"
+          inActiveStrokeColor="#4c6394"
+          inActiveStrokeOpacity={0.5}
+          inActiveStrokeWidth={20}
+          activeStrokeWidth={20}
+          title="Steps"
+          titleColor="#555"
+          titleFontSize={22}
+          titleStyle={{fontFamily: Fonts.SemiBold}}
+        />
+
+        <CustomText
+          fontSize={RFValue(8)}
+          fontFamily={Fonts.SemiBold}
+          style={styles.text}>
+          Start Walking, counter will update automatically.
+        </CustomText>
+      </View>
     </View>
   );
 };
@@ -35,10 +118,16 @@ const styles = StyleSheet.create({
     right: 10,
     top: 10,
   },
-  message: {
-    width: 400,
-    height: 400,
+  indicator: {
+    marginTop: 10,
+    marginBottom: 20,
     alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    marginTop: 20,
+    textAlign: 'center',
   },
 });
 
